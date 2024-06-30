946767c30858a4e3152b20d1e3291fd2705f4097
taiz <walter.kerly@protonmail.com>
2024-06-30 02:48:25 -0400

    feat(downloader): Add downloader module

--- downloader.py
+++ downloader.py
@@ -1,4 +1,9 @@
+"""Module to handle downloading and tagging of files"""
+
 from scraper import Scraper
+import os
+import urllib.request
+import ffmpeg
 
 # Site constants
 SITE_LINK = "https://musicforprogramming.net/latest/"  # The domain of the site
@@ -11,3 +16,43 @@ web_scraper = Scraper(WEBDRIVER_PATH, SITE_LINK, SOURCE_LINK)
 avail_titles = web_scraper.parse_available_titles()
 print(avail_titles)
 print([web_scraper.title_to_link(title) for title in avail_titles])
+
+
+class Downloader:
+    """docstring for Downloader."""
+
+    def __init__(self, link_list, directory_path, artist_name, album_title):
+        self.directory_path = directory_path
+        self.artist_name = artist_name
+        self.album_title = album_title
+        self.link_list = link_list
+
+    def download_links(self):
+        """Download the links"""
+        # Does the specified path exist? If not, create it
+        if not os.path.isdir(self.directory_path):
+            os.mkdir(self.directory_path)
+        os.chdir(self.directory_path)
+
+        # Download the files
+        for link in self.link_list:
+            filename = link.split("/")[-1]
+            urllib.request.urlretrieve(link, filename)
+
+        self.tag_links()
+
+    def tag_links(self):
+        """Tag the links"""
+        for song in self.link_list:
+            song_filename = song.split("/")[-1]
+            song_path = f"{self.directory_path}/{song_filename}"
+            tags = {
+                "metadata": f"title={song_filename.split('.')[0]}",
+                "metadata": f"artist={self.artist_name}",
+                "metadata": f"album={self.album_title}",
+            }
+            (
+                ffmpeg.input(song_path)
+                .output(song_path, **tags)
+                .run(overwrite_output=True)
+            )
